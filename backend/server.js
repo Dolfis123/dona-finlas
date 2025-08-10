661409dee128c0d9fec56ff210e31c6ea5aa2a63
Dolfis123 <kdolfis.com>
2025-08-10 18:06:00 +0900

    fg-1

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
 const dotenv = require("dotenv");
+const express = require("express");
 const path = require("path");
 const cors = require("cors"); // Impor CORS
 const { sequelize, syncDatabase } = require("./src/models");
