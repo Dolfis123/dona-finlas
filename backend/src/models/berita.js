a2e62d1daf0cc68291815dcf8456d8605508f941
Dolfis123 <kdolfis.com>
2025-10-18 19:06:32 +0900

    DONA-6

--- backend/src/models/berita.js
+++ backend/src/models/berita.js
@@ -26,9 +26,9 @@ const Berita = sequelize.define(
         },
         // --- KOLOM BARU DITAMBAHKAN DI SINI ---
         tanggal_publikasi: {
-            type: DataTypes.DATE, // Menggunakan tipe data DATE atau TIMESTAMP
-            allowNull: false,
-        },
+            type: DataTypes.DATETIME,
+            allowNull: true // Ubah ini
+        }
         // Kolom penulis_id akan dibuat oleh relasi di models/index.js
     }, {
         tableName: "berita",
