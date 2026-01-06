0d227d2a91489931f98cf9ee7cab973d81589933
Dolfis123 <kdolfis@gmail.com>
2026-01-06 14:43:11 +0900

    DONA-3

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,23 +1,18 @@
-// Mengimpor library dotenv untuk membaca file .env
-require("dotenv").config();
+require('dotenv').config();
 
-// Mengekspor konfigurasi database
 module.exports = {
-    // Konfigurasi untuk lingkungan pengembangan (development)
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        // Opsi tambahan untuk logging query SQL di console
-        logging: console.log,
-    },
-    // Anda bisa menambahkan konfigurasi untuk 'test' dan 'production' di sini nanti
-    test: {
-        // ...
-    },
-    production: {
-        // ...
-    },
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
 };
